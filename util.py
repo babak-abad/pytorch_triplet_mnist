9704aff04d5c2c16de8e632b7e1a9fc9a7efd212
add <babak.abad@gmail.com>
2024-07-18 14:42:41 +0330

    WIP for training embedding using triplet loss
    utl:
    - specified function (train_triplet) is added for triplet training
    
    train_embedding:
    - is added for viewing and loading training procedures

--- util.py
+++ util.py
@@ -49,9 +49,13 @@ class MNIST_DS(torch.utils.data.Dataset):
         r = random.randint(0, len(sub_dirs2) - 1)
         neg = os.path.join(dir2, sub_dirs2[r])
 
-        q = cv2.imread(query)
-        p = cv2.imread(pos)
-        n = cv2.imread(neg)
+        q = cv2.imread(query, cv2.IMREAD_GRAYSCALE).astype('double')
+        p = cv2.imread(pos, cv2.IMREAD_GRAYSCALE).astype('double')
+        n = cv2.imread(neg, cv2.IMREAD_GRAYSCALE).astype('double')
+
+        q = torch.from_numpy(q).unsqueeze(0)
+        p = torch.from_numpy(p).unsqueeze(0)
+        n = torch.from_numpy(n).unsqueeze(0)
 
         return q, p, n
 
@@ -121,6 +125,106 @@ def train(
 
     return trn_losses, vld_losses, best_state
 
+
+def train_triplet(
+    mdl,
+    train_dataloader,
+    valid_dataloader,
+    n_epoch,
+    opt,
+    criterion,
+    valid_step,
+    save_path):
+    min_valid_loss = float('inf')
+
+    best_state = 0
+
+    # save training and validation losses to draw them later
+    trn_losses = []
+    vld_losses = []
+
+    train_loss = 0.0
+    valid_loss = 0.0
+
+    for e in range(0, n_epoch):
+        mdl.train()
+        train_loss = 0.0
+        for _, (q, p, n) in enumerate(train_dataloader):
+            # opt.zero_grad()
+            # pred = mdl(inp)
+            # loss = criterion(pred, out)
+            # loss.backward()
+            # opt.step()
+            # train_loss += loss.item()
+
+            opt.zero_grad()
+            q = q.float()
+            p = p.float()
+            n = n.float()
+            pred_q = mdl(q)
+            pred_p = mdl(p)
+            pred_n = mdl(n)
+
+            loss = criterion(pred_q, pred_p, pred_n)
+            loss.backward()
+            opt.step()
+            train_loss += loss.item()
+
+        trn_losses.append(train_loss)
+
+        # if it is not time to calculate validation, continue
+        if e % valid_step != 0 and e != 0:
+            vld_losses.append(valid_loss)
+            continue
+
+        mdl.eval()
+        valid_loss = 0.0
+        with torch.no_grad():
+            for i, (q, p, n) in enumerate(valid_dataloader):
+                q = q.float()
+                p = p.float()
+                n = n.float()
+
+                pred_q = mdl(q)
+                pred_p = mdl(p)
+                pred_n = mdl(n)
+                ls = criterion(pred_q, pred_p, pred_n)
+                valid_loss += ls.item()
+
+        vld_losses.append(valid_loss)
+
+        # if they are better weights save them (less loss = better weights)
+        if valid_loss < min_valid_loss:
+            best_state = mdl.state_dict()
+            if save_path != '':
+                p = save_path.format(
+                    en=n_epoch,
+                    vl=valid_loss,
+                    tl=train_loss)
+                torch.save(mdl, p)
+
+        print('epoch: ' + str(e + 1))
+        print('train loss: ' + str(train_loss / len(train_dataloader)))
+        print('valid loss: ' + str(valid_loss / len(valid_dataloader)))
+        print('\n')
+
+    return trn_losses, vld_losses, best_state
+
+
+class TripletLoss(nn.Module):
+    def __init__(self, margin=1.0):
+        super(TripletLoss, self).__init__()
+        self.margin = margin
+
+    def calc_euclidean(self, x1, x2):
+        return (x1 - x2).pow(2).sum(1)
+
+    def forward(self, anchor: torch.Tensor, positive: torch.Tensor, negative: torch.Tensor) -> torch.Tensor:
+        distance_positive = self.calc_euclidean(anchor, positive)
+        distance_negative = self.calc_euclidean(anchor, negative)
+        losses = torch.relu(distance_positive - distance_negative + self.margin)
+
+        return losses.mean()
 
 # if idx = 0 then it prints x
 # if idx = 1 then it prints y
